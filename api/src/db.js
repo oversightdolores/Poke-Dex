da4c220111e2f522f67585a3812b7417aea24891
oversightdolores <oversightdolores@gmail.com>
2022-11-13 21:21:17 -0300

    configDb

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE, PG_URL
+  PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE,
 } = process.env;
 
 const sequelize = new Sequelize(`postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`, {
