af458008027d6f304e7080b86603c853ada5feba
oversightdolores <oversightdolores@gmail.com>
2022-11-14 12:38:43 -0300

    dep2

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
 ReactDOM.render(
   <React.StrictMode>
