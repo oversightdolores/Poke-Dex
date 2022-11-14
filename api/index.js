72cf388db0f08e448dbb02c60cb3d7f5e3704a89
oversightdolores <oversightdolores@gmail.com>
2022-11-14 11:34:00 -0300

    envp

--- api/index.js
+++ api/index.js
@@ -21,12 +21,12 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 
-const PORT = process.env.PORT || 3001;
+const PG_PORT = process.env.PG_PORT || 3001;
 
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(PG_PORT, () => {
+    console.log(`%s listening at ${PG_PORT}`); // eslint-disable-line no-console
   });
 });
